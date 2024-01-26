3e5239e267a751c3847fafea29f8f736b80231ef
a0z9 <36224056+a0z9@users.noreply.github.com>
2024-01-26 12:14:32 +0300

    Update install_zabbix_proxy_6.4_ubuntu_20.04_sqlite3.sh

--- install_zabbix_proxy_6.4_ubuntu_20.04_sqlite3.sh
+++ install_zabbix_proxy_6.4_ubuntu_20.04_sqlite3.sh
@@ -4,7 +4,7 @@
 
 sudo apt update
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
-dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
+sudo dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
 sudo apt update
 
 # Zabbix сервер, веб-интерфейс и агент
@@ -17,7 +17,7 @@ sudo mkdir -p /var/lib/sqlite/
 sudo chown -R zabbix:zabbix /var/lib/sqlite
 
 sudo sh -c "echo DBName=/var/lib/sqlite/zabbix_proxy.db >> /etc/zabbix/zabbix_proxy.conf"
-sudo sed -i 's/Server=127.0.0.1/Server=172.17.17.11/g' /etc/zabbix/zabbix_proxy.conf
+sudo sed -i 's/Server=127.0.0.1/Server=192.168.56.11/g' /etc/zabbix/zabbix_proxy.conf
 
 sudo systemctl restart zabbix-proxy
 sudo systemctl enable zabbix-proxy
